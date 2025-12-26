41cc3ee7f041d2471d0f6588495df298baf5e441
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-26 08:40:39 -0300

    feat: modifies methods responses and add ObjectId to ids in backend/src/controllers/pets.controller.ts

--- backend/src/controllers/pets.controller.ts
+++ backend/src/controllers/pets.controller.ts
@@ -1,59 +1,55 @@
 import { Request, Response } from "express";
+import { ObjectId } from "mongodb";
 import petsModel from "../models/pets.model";
 
 class PetsController {
-  async create(req: Request, res: Response) {
-    try {
-      const pet = req.body;
-      const newPet = await petsModel.create(pet);
-      res.status(201).json({ message: "create", status: "ok", newPet });
-    } catch (err) {
-      res.status(500).json({ message: "create", status: "error" });
-    }
-  }
-
-  async get(req: Request, res: Response) {
-    try {
-      const petId = req.params.id;
-      const pet = await petsModel.findById(petId);
-      res.status(200).json({ message: "get", status: "ok", pet });
-    } catch (err) {
-      res.status(500).json({ message: "get", status: "error" });
-    }
-  }
-
-  async update(req: Request, res: Response) {
-    try {
-      res.status(200).json({ message: "update", status: "ok" });
-    } catch (err) {
-      res.status(500).json({ message: "update", status: "error" });
-    }
-  }
-
-  async delete(req: Request, res: Response) {
-    try {
-      res.status(200).json({ message: "delete", status: "ok" });
-    } catch (err) {
-      res.status(500).json({ message: "delete", status: "error" });
-    }
-  }
-
   async getAll(req: Request, res: Response) {
     try {
       const pets = await petsModel.getAll();
-      res.status(200).json({ message: "getAll", status: "ok", pets });
+      res.status(200).json({ status: "ok", pets });
     } catch (err) {
-      res.status(500).json({ message: "getAll", status: "error" });
+      res.status(500).json({ status: "error" });
     }
   }
 
   async getById(req: Request, res: Response) {
     try {
-      const petId = req.params.id;
-      const pet = await petsModel.findById(petId);
-      res.status(200).json({ message: "getById", status: "ok", pet });
+      const { id } = req.params;
+      const pet = await petsModel.findById(new ObjectId(id));
+      res.status(200).json({ status: "ok", pet });
     } catch (err) {
-      res.status(500).json({ message: "getById", status: "error" });
+      res.status(500).json({ status: "error" });
+    }
+  }
+
+  async create(req: Request, res: Response) {
+    try {
+      const pet = req.body;
+      const newPet = await petsModel.create(pet);
+      res.status(201).json({ status: "ok", newPet });
+    } catch (err) {
+      res.status(500).json({ status: "error" });
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const pet = req.body;
+      const updatedPet = await petsModel.update(new ObjectId(id), pet);
+      res.status(200).json({ status: "ok", updatedPet });
+    } catch (err) {
+      res.status(500).json({ status: "error" });
+    }
+  }
+
+  async delete(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const deletedPet = await petsModel.delete(new ObjectId(id));
+      res.status(200).json({ status: "ok", deletedPet });
+    } catch (err) {
+      res.status(500).json({ status: "error" });
     }
   }
 }
