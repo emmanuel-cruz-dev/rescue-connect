09debb54efcec8067eecfb8e086e07f3cd838d64
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-26 07:35:43 -0300

    feat: adds petsRouter and express urlencoded to backend/src/app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import petsRouter from "./routes/pets.routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (_req, res) => {
-  res.json({ message: "API is running" });
+  res.json({ message: "API is running 🚀" });
 });
+
+app.use("/pets", petsRouter);
 
 export default app;
