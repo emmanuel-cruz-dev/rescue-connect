76b901de6c25219c498f520842a750af5b1f2e9a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-26 07:34:40 -0300

    feat: adds dotenv and try/catch block to backend/src/index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,15 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 import app from "./app";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port: http://localhost:${PORT}`);
-});
+try {
+  app.listen(PORT, () => {
+    console.log(`Server running on port: http://localhost:${PORT}`);
+  });
+} catch (err) {
+  console.error(err);
+}
