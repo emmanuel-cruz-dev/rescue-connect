e34f1de0d351e6395aa358baea45fe556ca98ef0
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-26 11:00:55 -0300

    feat: adds pet.types to src/types/

---
+++ backend/src/types/pet.types.ts
@@ -0,0 +1,18 @@
+import { Document, Types } from "mongoose";
+
+type PetType = "perro" | "gato" | "conejo" | "ave";
+
+export interface IPet {
+  name: string;
+  type: PetType;
+  age: number;
+  breed: string;
+  description?: string;
+  adopted: boolean;
+}
+
+export interface IPetDocument extends IPet, Document {
+  _id: Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
