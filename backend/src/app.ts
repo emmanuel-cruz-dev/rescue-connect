33f77e0b322c02b965db9403f5a71ba15dccbb3b
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-28 10:07:57 -0300

    feat: adds swagger imports to backend/src/app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,18 @@
+import "./docs/swagger";
+import "./docs/routes.swagger";
+
 import express from "express";
 import petsRouter from "./routes/pets.routes";
 import authRouter from "./routes/auth.routes";
 import { errorHandler } from "./middlewares/error.middleware";
+import { swaggerUiHandler, swaggerUiServe } from "./docs/swagger";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/docs", swaggerUiServe, swaggerUiHandler);
 
 app.get("/", (_req, res) => {
   res.json({ message: "API is running 🚀" });
