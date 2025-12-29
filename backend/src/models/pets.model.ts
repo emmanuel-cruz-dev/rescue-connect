9891188a214fbcc6df98367b81af3c08115a8339
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-29 07:24:15 -0300

    feat: modifies CRUD methods in backend/src/models/pets.model.ts

--- backend/src/models/pets.model.ts
+++ backend/src/models/pets.model.ts
@@ -1,70 +1,44 @@
-import { ObjectId } from "mongodb";
-import dbClient from "../config/dbClient.config";
-
-interface Pet {
-  _id: ObjectId;
-  name: string;
-  type: string;
-  age: number;
-  breed: string;
-}
+import { Types } from "mongoose";
+import petModel from "../schemas/pets.schema";
+import { IPet } from "../types/pet.types";
 
 class PetsModel {
   async getAll() {
-    try {
-      const db = await dbClient.ensureConnection();
-      const pets = await db.collection("pets").find().toArray();
-      return pets;
-    } catch (err) {
-      console.error("Error getting all pets", err);
-      throw new Error("Error getting all pets");
-    }
+    return await petModel.find();
   }
 
-  async findById(id: ObjectId) {
-    try {
-      const db = await dbClient.ensureConnection();
-      const pet = await db.collection("pets").findOne({ _id: id });
-      return pet;
-    } catch (err) {
-      console.error("Error finding pet by id", err);
-      throw new Error("Error finding pet by id");
+  async findById(id: string | Types.ObjectId) {
+    const pet = await petModel.findById(id);
+
+    if (!pet) {
+      throw new Error("Pet not found");
     }
+    return pet;
   }
 
-  async create(pet: Pet) {
-    try {
-      const db = await dbClient.ensureConnection();
-      const newPet = await db.collection("pets").insertOne(pet);
-      return newPet;
-    } catch (err) {
-      console.error("Error creating pet", err);
-      throw new Error("Error creating pet");
-    }
+  async create(petData: IPet) {
+    return await petModel.create(petData);
   }
 
-  async update(id: ObjectId, pet: Pet) {
-    try {
-      const db = await dbClient.ensureConnection();
-      const updatedPet = await db
-        .collection("pets")
-        .updateOne({ _id: id }, { $set: pet });
-      return updatedPet;
-    } catch (err) {
-      console.error("Error updating pet", err);
-      throw new Error("Error updating pet");
+  async update(id: string | Types.ObjectId, petData: Partial<IPet>) {
+    const pet = await petModel.findByIdAndUpdate({ _id: id }, petData, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!pet) {
+      throw new Error("Pet not found");
     }
+    return pet;
   }
 
-  async delete(id: ObjectId) {
-    try {
-      const db = await dbClient.ensureConnection();
-      const deletedPet = await db.collection("pets").deleteOne({ _id: id });
-      return deletedPet;
-    } catch (err) {
-      console.error("Error deleting pet", err);
-      throw new Error("Error deleting pet");
+  async delete(id: string | Types.ObjectId) {
+    const pet = await petModel.findOneAndDelete({ _id: id });
+
+    if (!pet) {
+      throw new Error("Pet not found");
     }
+    return pet;
   }
 }
 
