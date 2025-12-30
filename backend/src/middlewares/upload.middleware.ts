1bd02f3494988f69487713b36f7820959b1718d2
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-30 08:15:21 -0300

    feat: adds upload.middleware.ts to backend/src/middlewares/

---
+++ backend/src/middlewares/upload.middleware.ts
@@ -0,0 +1,22 @@
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req: any, file: Express.Multer.File, cb: any) => {
+  const allowedTypes = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
+
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Formato de imagen no válido. Use JPG, PNG o WEBP"), false);
+  }
+};
+
+export const upload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+    files: 5,
+  },
+});
