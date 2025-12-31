9ae716a97ab9a7fc423ef7f3b35b0efd2ad05fb1
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-31 08:29:57 -0300

    feat: adds SharedModule to frontend/src/app/app.ts

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SharedModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('rescue-connect');
