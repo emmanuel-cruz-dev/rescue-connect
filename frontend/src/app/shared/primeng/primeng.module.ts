513f605bbfffaf6fff8ce34818f60f6f4f16f7dd
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-31 08:28:14 -0300

    feat: adds primeng.module to frontend/src/app/shared/primeng/

---
+++ frontend/src/app/shared/primeng/primeng.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { FileUploadModule } from 'primeng/fileupload';
+import { GalleriaModule } from 'primeng/galleria';
+import { ChipModule } from 'primeng/chip';
+import { TagModule } from 'primeng/tag';
+import { PaginatorModule } from 'primeng/paginator';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToolbarModule } from 'primeng/toolbar';
+import { MenuModule } from 'primeng/menu';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { RippleModule } from 'primeng/ripple';
+import { DividerModule } from 'primeng/divider';
+
+const PRIMENG_MODULES = [
+  ButtonModule,
+  CardModule,
+  TableModule,
+  ToastModule,
+  DialogModule,
+  InputTextModule,
+  PasswordModule,
+  FileUploadModule,
+  GalleriaModule,
+  ChipModule,
+  TagModule,
+  PaginatorModule,
+  ConfirmDialogModule,
+  ToolbarModule,
+  MenuModule,
+  ProgressSpinnerModule,
+  MenubarModule,
+  BadgeModule,
+  AvatarModule,
+  RippleModule,
+  DividerModule,
+];
+
+@NgModule({
+  imports: [PRIMENG_MODULES],
+  exports: [PRIMENG_MODULES],
+})
+export class PrimeNgModule {}
