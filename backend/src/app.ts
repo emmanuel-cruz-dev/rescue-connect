4d760e28b8c643f32305f6dda8a6041c0f02f14a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-07 08:39:30 -0300

    feat: adds apiRouter to backend/src/app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,24 +2,19 @@ import "./docs/swagger";
 import "./docs/routes.swagger";
 
 import express from "express";
-import petsRouter from "./routes/pets.routes";
-import authRouter from "./routes/auth.routes";
 import { errorHandler } from "./middlewares/error.middleware";
-import { swaggerUiHandler, swaggerUiServe } from "./docs/swagger";
+import apiRouter from "./routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1/docs", swaggerUiServe, swaggerUiHandler);
+app.use("/api/v1", apiRouter);
 
 app.get("/", (_req, res) => {
   res.json({ message: "API is running 🚀" });
 });
-
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/pets", petsRouter);
 
 app.use(errorHandler);
 
