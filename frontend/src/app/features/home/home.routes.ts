9ae70508c641a1dba51e9583f137f6bc48febb37
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-08 16:20:40 -0300

    feat: adds Landing to frontend/src/app/features/home/home.routes.ts

---
+++ frontend/src/app/features/home/home.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { Landing } from './pages/landing/landing';
+
+export const HOME_ROUTES: Routes = [
+  {
+    path: '',
+    component: Landing,
+  },
+];
