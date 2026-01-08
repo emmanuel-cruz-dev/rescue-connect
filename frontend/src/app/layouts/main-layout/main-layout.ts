edc5f7e4ca6b1d7798981defcbaf570006f4685e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-08 16:15:05 -0300

    feat: adds main-layout files to frontend/src/app/layouts/

---
+++ frontend/src/app/layouts/main-layout/main-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [SharedModule],
+  templateUrl: './main-layout.html',
+})
+export class MainLayout {}
