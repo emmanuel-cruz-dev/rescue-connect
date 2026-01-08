36b1d560bd8790c0cc8b83ffb981d7b7e0593d8b
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-08 14:24:52 -0300

    feat: adds pet-gallery files to frontend/src/app/features/pets/components/pet-gallery/

---
+++ frontend/src/app/features/pets/components/pet-gallery/pet-gallery.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-gallery',
+  templateUrl: './pet-gallery.html',
+  styleUrl: './pet-gallery.css',
+  standalone: false,
+})
+export class PetGallery {}
