ab383cf84e99b8ad142fda91760b5e00a6c36491
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-09 08:58:12 -0300

    feat: adds hero-section, featured-pets, how-it-works and testimonials to frontend/src/app/features/home/pages/landing/landing.ts

--- frontend/src/app/features/home/pages/landing/landing.ts
+++ frontend/src/app/features/home/pages/landing/landing.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroSection } from '../../components/hero-section/hero-section';
+import { FeaturedPets } from '../../components/featured-pets/featured-pets';
+import { HowItWorks } from '../../components/how-it-works/how-it-works';
+import { Testimonials } from '../../components/testimonials/testimonials';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [HeroSection, FeaturedPets, HowItWorks, Testimonials],
   templateUrl: './landing.html',
-  styleUrl: './landing.css',
 })
-export class Landing {
+export class Landing implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
 }
