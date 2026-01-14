c8c81839b2651c4f1b97739a8c9064412e7325ca
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-14 13:21:44 -0300

    feat: adds new breakpoints to responsiveOptions in frontend/src/app/features/home/components/featured-pets/featured-pets.ts

--- frontend/src/app/features/home/components/featured-pets/featured-pets.ts
+++ frontend/src/app/features/home/components/featured-pets/featured-pets.ts
@@ -21,14 +21,24 @@ export class FeaturedPets implements OnInit {
 
     this.responsiveOptions = [
       {
-        breakpoint: '1024px',
+        breakpoint: '1400px',
         numVisible: 3,
         numScroll: 3,
       },
       {
-        breakpoint: '768px',
+        breakpoint: '1200px',
         numVisible: 2,
         numScroll: 2,
+      },
+      {
+        breakpoint: '1024px',
+        numVisible: 2,
+        numScroll: 2,
+      },
+      {
+        breakpoint: '768px',
+        numVisible: 1,
+        numScroll: 1,
       },
       {
         breakpoint: '560px',
