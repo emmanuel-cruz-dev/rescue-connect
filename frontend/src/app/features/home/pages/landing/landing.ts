6cf973054108a28c08affa0793f2d1fd6416830b
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-02-12 15:09:12 -0300

    feat: adds Faq component to landing files in frontend/src/app/features/home/pages/landing/

--- frontend/src/app/features/home/pages/landing/landing.ts
+++ frontend/src/app/features/home/pages/landing/landing.ts
@@ -3,10 +3,11 @@ import { HeroSection } from '../../components/hero-section/hero-section';
 import { FeaturedPets } from '../../components/featured-pets/featured-pets';
 import { HowItWorks } from '../../components/how-it-works/how-it-works';
 import { Testimonials } from '../../components/testimonials/testimonials';
+import { Faq } from '../../components/faq/faq';
 
 @Component({
   selector: 'app-landing',
-  imports: [HeroSection, FeaturedPets, HowItWorks, Testimonials],
+  imports: [HeroSection, FeaturedPets, HowItWorks, Testimonials, Faq],
   templateUrl: './landing.html',
 })
 export class Landing implements OnInit {
