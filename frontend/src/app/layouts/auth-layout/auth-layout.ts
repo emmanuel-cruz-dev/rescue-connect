f6377d1a67c58184b6d9ab6c4e304b69f202dfa2
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-02-13 11:48:58 -0300

    feat: adds Navbar to auth-layout files in frontend/src/app/layouts/auth-layout/

--- frontend/src/app/layouts/auth-layout/auth-layout.ts
+++ frontend/src/app/layouts/auth-layout/auth-layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Navbar } from '../../shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [RouterModule],
+  imports: [RouterModule, Navbar],
   templateUrl: './auth-layout.html',
 })
 export class AuthLayout {}
