78bdb8f71a4ca17277703255fa4ce618144e9fa9
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-02-15 08:13:21 -0300

    feat: adds router module to empty-layout files in frontend/src/app/layouts/empty-layout/

--- frontend/src/app/layouts/empty-layout/empty-layout.ts
+++ frontend/src/app/layouts/empty-layout/empty-layout.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-empty-layout',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './empty-layout.html',
 })
 export class EmptyLayout {}
