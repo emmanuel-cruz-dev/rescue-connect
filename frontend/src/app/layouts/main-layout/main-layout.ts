83927345e98a083244b1fefa35a1b9fcbaff0f0c
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-02-19 11:19:22 -0300

    feat: adds ScrollToTop to main-layout .ts and .html files in frontend/src/app/layouts/main-layout/

--- frontend/src/app/layouts/main-layout/main-layout.ts
+++ frontend/src/app/layouts/main-layout/main-layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Footer } from '../../shared/components/footer/footer';
 import { Navbar } from '../../shared/components/navbar/navbar';
+import { ScrollToTop } from '../../shared/components/scroll-to-top/scroll-to-top';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterModule, Navbar, Footer],
+  imports: [RouterModule, Navbar, Footer, ScrollToTop],
   templateUrl: './main-layout.html',
 })
 export class MainLayout {}
