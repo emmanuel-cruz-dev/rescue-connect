842b1bcbf4137f3e6939366dbf6a529ac13ffa46
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-02-23 09:48:05 -0300

    feat: adds scroll methods to frontend/src/app/shared/components/navbar/navbar.ts

--- frontend/src/app/shared/components/navbar/navbar.ts
+++ frontend/src/app/shared/components/navbar/navbar.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive, Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { MenuItem } from 'primeng/api';
 import { ThemeService } from '../../../core/services/theme.service';
 import { PRIMENG_IMPORTS } from '../../primeng/primeng.imports';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [PRIMENG_IMPORTS, RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, PRIMENG_IMPORTS],
   templateUrl: './navbar.html',
 })
 export class Navbar implements OnInit {
   items: MenuItem[] = [];
 
-  constructor(public themeService: ThemeService) {}
+  constructor(public themeService: ThemeService, private router: Router) {}
 
   ngOnInit() {
     this.items = [
@@ -29,14 +30,36 @@ export class Navbar implements OnInit {
       {
         label: 'Cómo funciona',
         icon: 'pi pi-info-circle',
-        routerLink: '/how-it-works',
-      },
-      {
-        label: 'Refugios',
-        icon: 'pi pi-building',
-        routerLink: '/shelters',
+        command: () => this.scrollToSection('how-it-works'),
       },
     ];
+
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
+      const fragment = this.router.parseUrl(this.router.url).fragment;
+      if (fragment) {
+        this.scrollToElementWithDelay(fragment, 300);
+      }
+    });
+  }
+
+  scrollToSection(sectionId: string) {
+    if (this.router.url !== '/') {
+      this.router.navigate(['/'], { fragment: sectionId });
+    } else {
+      this.scrollToElementWithDelay(sectionId, 100);
+    }
+  }
+
+  private scrollToElementWithDelay(sectionId: string, delay: number = 100) {
+    setTimeout(() => {
+      const element = document.getElementById(sectionId);
+      if (element) {
+        element.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+      }
+    }, delay);
   }
 
   toggleTheme() {
