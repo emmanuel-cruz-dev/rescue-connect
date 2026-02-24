d9a49c1b20e5672fed6bffda3923cb85438f8d29
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-02-24 08:41:35 -0300

    feat: removes scroll methods and how-it-works link in frontend/src/app/shared/components/navbar/navbar.ts

--- frontend/src/app/shared/components/navbar/navbar.ts
+++ frontend/src/app/shared/components/navbar/navbar.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink, RouterLinkActive, Router, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { ThemeService } from '../../../core/services/theme.service';
 import { PRIMENG_IMPORTS } from '../../primeng/primeng.imports';
@@ -13,7 +12,7 @@ import { PRIMENG_IMPORTS } from '../../primeng/primeng.imports';
 export class Navbar implements OnInit {
   items: MenuItem[] = [];
 
-  constructor(public themeService: ThemeService, private router: Router) {}
+  constructor(public themeService: ThemeService) {}
 
   ngOnInit() {
     this.items = [
@@ -27,39 +26,7 @@ export class Navbar implements OnInit {
         icon: 'custom__pet-icon',
         routerLink: '/pets',
       },
-      {
-        label: 'Cómo funciona',
-        icon: 'pi pi-info-circle',
-        command: () => this.scrollToSection('how-it-works'),
-      },
     ];
-
-    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
-      const fragment = this.router.parseUrl(this.router.url).fragment;
-      if (fragment) {
-        this.scrollToElementWithDelay(fragment, 300);
-      }
-    });
-  }
-
-  scrollToSection(sectionId: string) {
-    if (this.router.url !== '/') {
-      this.router.navigate(['/'], { fragment: sectionId });
-    } else {
-      this.scrollToElementWithDelay(sectionId, 100);
-    }
-  }
-
-  private scrollToElementWithDelay(sectionId: string, delay: number = 100) {
-    setTimeout(() => {
-      const element = document.getElementById(sectionId);
-      if (element) {
-        element.scrollIntoView({
-          behavior: 'smooth',
-          block: 'start',
-        });
-      }
-    }, delay);
   }
 
   toggleTheme() {
