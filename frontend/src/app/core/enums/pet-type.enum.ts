8d3481247928e5c489723c4c65c251dc37889955
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-02-25 10:46:50 -0300

    feat: adds PetSortableFields to frontend/src/app/core/enums/pet-type.enum.ts

--- frontend/src/app/core/enums/pet-type.enum.ts
+++ frontend/src/app/core/enums/pet-type.enum.ts
@@ -3,3 +3,5 @@ export type PetType = 'perro' | 'gato';
 export type PetGender = 'macho' | 'hembra';
 
 export type PetSize = 'pequeño' | 'mediano' | 'grande' | 'extra grande';
+
+export type PetSortableFields = 'createdAt' | 'name' | 'birthDate' | 'size';
