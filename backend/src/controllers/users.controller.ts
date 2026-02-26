13782649049af5880a835f71e4415ff98a2739f1
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-02-26 10:16:32 -0300

    feat: adds users.controller.ts to backend/src/controllers/

---
+++ backend/src/controllers/users.controller.ts
@@ -0,0 +1,87 @@
+import { Request, Response, NextFunction } from "express";
+import usersModel from "../models/user.model";
+import { IUserQueryParams } from "../types";
+
+class UsersController {
+  async getAll(req: Request, res: Response, next: NextFunction) {
+    try {
+      const queryParams = req.query as unknown as IUserQueryParams;
+      const result = await usersModel.getAll(queryParams);
+
+      res.status(200).json({
+        status: "success",
+        data: result.data,
+        pagination: result.pagination,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async getById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const user = await usersModel.findById(id);
+      res.status(200).json({ status: "success", data: { user } });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const newUser = await usersModel.create(req.body);
+      res.status(201).json({ status: "success", data: { user: newUser } });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const updatedUser = await usersModel.update(id, req.body);
+      res.status(200).json({ status: "success", data: { user: updatedUser } });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async updatePassword(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const { currentPassword, newPassword } = req.body;
+      await usersModel.updatePassword(id, currentPassword, newPassword);
+      res.status(200).json({
+        status: "success",
+        message: "Contraseña actualizada exitosamente",
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async deactivate(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const user = await usersModel.deactivate(id);
+      res.status(200).json({ status: "success", data: { user } });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async delete(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      await usersModel.delete(id);
+      res
+        .status(200)
+        .json({ status: "success", message: "Usuario eliminado exitosamente" });
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+export default new UsersController();
