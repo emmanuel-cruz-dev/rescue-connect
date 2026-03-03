83b5e5749e0dd71679b628b12208010c9084be05
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-03 10:03:58 -0300

    feat: removes ScrollToTop in main-layout files in frontend/src/app/layouts/main-layout/

--- frontend/src/app/layouts/main-layout/main-layout.ts
+++ frontend/src/app/layouts/main-layout/main-layout.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Footer } from '../../shared/components/footer/footer';
 import { Navbar } from '../../shared/components/navbar/navbar';
-import { ScrollToTop } from '../../shared/components/scroll-to-top/scroll-to-top';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterModule, Navbar, Footer, ScrollToTop],
+  imports: [RouterModule, Navbar, Footer],
   templateUrl: './main-layout.html',
 })
 export class MainLayout {}
