b7422dd212cd520363c583b6627ef0ed08bb25ed
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-03 10:25:28 -0300

    feat: adds scroll to top to app.ts and app.html in frontend/src/app/

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Toast } from 'primeng/toast';
+import { ScrollToTop } from './shared/components/scroll-to-top/scroll-to-top';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Toast],
+  imports: [RouterOutlet, Toast, ScrollToTop],
   templateUrl: './app.html',
 })
 export class App {}
