47760775a15a8eab25669446757c57449a859383
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-03 11:29:15 -0300

    feat: adds index.ts to frontend/src/app/core/models/

---
+++ frontend/src/app/core/models/index.ts
@@ -0,0 +1,6 @@
+export * from './adoption-request.model';
+export * from './api-response.model';
+export * from './auth-response.model';
+export * from './auth.model';
+export * from './pet.model';
+export * from './user.model';
