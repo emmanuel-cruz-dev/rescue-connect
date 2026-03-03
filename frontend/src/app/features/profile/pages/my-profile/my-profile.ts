3c737a17e092d64a41c89226e8235b280f388f41
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-03 10:05:23 -0300

    feat: removes RouterModule in frontend/src/app/features/profile/pages/my-profile/my-profile.ts

--- frontend/src/app/features/profile/pages/my-profile/my-profile.ts
+++ frontend/src/app/features/profile/pages/my-profile/my-profile.ts
@@ -1,11 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { AuthService } from '../../../../core/services/auth.service';
 import { PRIMENG_IMPORTS } from '../../../../shared/primeng/primeng.imports';
 
 @Component({
   selector: 'app-my-profile',
-  imports: [RouterModule, PRIMENG_IMPORTS],
+  imports: [PRIMENG_IMPORTS],
   templateUrl: './my-profile.html',
 })
 export class MyProfile {
