ac6aeea717a92e127cdd5aa9047b022d3853b774
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-04 08:11:22 -0300

    refactor: modifies services imports in frontend/src/app/features/profile/pages/my-profile/my-profile.ts

--- frontend/src/app/features/profile/pages/my-profile/my-profile.ts
+++ frontend/src/app/features/profile/pages/my-profile/my-profile.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { AuthService } from '../../../../core/services/auth.service';
+import { AuthService } from '../../../../core/services';
 import { PRIMENG_IMPORTS } from '../../../../shared/primeng/primeng.imports';
 
 @Component({
