e7c0465d0f66a3b4bc6cca3672bcdf847df8765b
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-05 13:31:07 -0300

    feat: modifies frontend/src/app/features/profile/pages/my-adoptions/my-adoptions.ts

--- frontend/src/app/features/profile/pages/my-adoptions/my-adoptions.ts
+++ frontend/src/app/features/profile/pages/my-adoptions/my-adoptions.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../../core/services';
+import { PetCard } from '../../../pets/components/pet-card/pet-card';
+import { PetCardSkeleton } from '../../../../shared/components/pet-card-skeleton/pet-card-skeleton';
 import { PRIMENG_IMPORTS } from '../../../../shared/primeng/primeng.imports';
+import { IPet } from '../../../../core/models';
 
 @Component({
   selector: 'app-my-adoptions',
-  imports: [RouterModule, PRIMENG_IMPORTS],
+  imports: [RouterModule, PetCard, PetCardSkeleton, PRIMENG_IMPORTS],
   templateUrl: './my-adoptions.html',
 })
-export class MyAdoptions {}
+export class MyAdoptions implements OnInit {
+  private authService = inject(AuthService);
+
+  pets = signal<IPet[]>([]);
+  loading = signal<boolean>(false);
+
+  ngOnInit(): void {
+    this.loading.set(true);
+    this.authService.getMyPets().subscribe({
+      next: (response) => {
+        if (Array.isArray(response?.data?.pets)) {
+          this.pets.set(response?.data?.pets);
+        }
+        this.loading.set(false);
+      },
+      error: () => {
+        this.loading.set(false);
+        throw new Error('Error al obtener las mascotas adoptadas');
+      },
+    });
+  }
+}
