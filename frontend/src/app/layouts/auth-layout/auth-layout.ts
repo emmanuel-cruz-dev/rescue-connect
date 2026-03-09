e36502d1bb199111489682b17d17056bb61c546d
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-09 14:20:38 -0300

    feat: adds prime ng imports to frontend/src/app/layouts/auth-layout/auth-layout.ts

--- frontend/src/app/layouts/auth-layout/auth-layout.ts
+++ frontend/src/app/layouts/auth-layout/auth-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Navbar } from '../../shared/components/navbar/navbar';
+import { PRIMENG_IMPORTS } from '../../shared/primeng/primeng.imports';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [RouterModule, Navbar],
+  imports: [RouterModule, Navbar, PRIMENG_IMPORTS],
   templateUrl: './auth-layout.html',
 })
 export class AuthLayout {}
