c5acca070891ce6e88d685b8720031b625d99bb1
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-09 10:25:56 -0300

    feat: adds adoption service and component to frontend/src/app/layouts/main-layout/main-layout.ts

--- frontend/src/app/layouts/main-layout/main-layout.ts
+++ frontend/src/app/layouts/main-layout/main-layout.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AdoptionFlowService } from '../../features/adoptions/services/adoption-flow.service';
 import { Footer } from '../../shared/components/footer/footer';
 import { Navbar } from '../../shared/components/navbar/navbar';
+import { AdoptionDialog } from '../../shared/components/adoption-dialog/adoption-dialog';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterModule, Navbar, Footer],
+  imports: [RouterModule, Navbar, Footer, AdoptionDialog],
   templateUrl: './main-layout.html',
 })
-export class MainLayout {}
+export class MainLayout {
+  adoptionFlow = inject(AdoptionFlowService);
+}
