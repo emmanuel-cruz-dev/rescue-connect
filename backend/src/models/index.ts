f7d2c92446d8c8af7df71e8558301b5a23b1099a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-12 11:38:42 -0300

    feat: adds index.ts to backend/src/models/

---
+++ backend/src/models/index.ts
@@ -0,0 +1,5 @@
+export { default as adminModel } from "./admin.model";
+export { default as adoptionModel } from "./adoption.model";
+export { default as authModel } from "./auth.model";
+export { default as petModel } from "./pets.model";
+export { default as userModel } from "./user.model";
