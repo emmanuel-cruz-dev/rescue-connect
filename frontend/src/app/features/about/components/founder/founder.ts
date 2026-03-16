e1b2ebedb9490d2386b61eb1a726346b4eaf8799
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-16 15:39:56 -0300

    feat: adds founder.ts to frontend/src/app/features/about/components/founder/

---
+++ frontend/src/app/features/about/components/founder/founder.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { PRIMENG_IMPORTS } from '../../../../shared/primeng/primeng.imports';
+
+@Component({
+  selector: 'app-founder',
+  imports: [PRIMENG_IMPORTS],
+  templateUrl: './founder.html',
+})
+export class Founder {}
