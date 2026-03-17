ee0e984a803bd40d0bf61713a7a7ccf42ae416c8
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-17 11:42:27 -0300

    feat: modifies styles in frontend/src/app/features/home/components/hero-section/hero-section.html

--- frontend/src/app/features/home/components/hero-section/hero-section.ts
+++ frontend/src/app/features/home/components/hero-section/hero-section.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
 import { PRIMENG_IMPORTS } from '../../../../shared/primeng/primeng.imports';
 
 @Component({
