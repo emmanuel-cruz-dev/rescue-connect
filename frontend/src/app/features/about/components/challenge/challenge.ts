edf9607edd077b0c1a985baf940be74932cccdd6
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-17 10:37:52 -0300

    feat: adds animations and modifies styles in challenge files in frontend/src/app/features/about/components/challenge/

--- frontend/src/app/features/about/components/challenge/challenge.ts
+++ frontend/src/app/features/about/components/challenge/challenge.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
+import { PRIMENG_IMPORTS } from '../../../../shared/primeng/primeng.imports';
+
 @Component({
   selector: 'app-challenge',
-  imports: [],
+  imports: [PRIMENG_IMPORTS],
   templateUrl: './challenge.html',
 })
 export class Challenge {}
