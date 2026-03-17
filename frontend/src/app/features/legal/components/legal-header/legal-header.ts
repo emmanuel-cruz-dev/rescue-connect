007b7052afea0dc7ce6f9f4fca12b0eef7879bef
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-17 13:36:41 -0300

    feat: adds legal-header files to frontend/src/app/features/legal/components/legal-header/

---
+++ frontend/src/app/features/legal/components/legal-header/legal-header.ts
@@ -0,0 +1,15 @@
+import { Component, input } from '@angular/core';
+
+import { PRIMENG_IMPORTS } from '../../../../shared/primeng/primeng.imports';
+
+@Component({
+  selector: 'app-legal-header',
+  imports: [PRIMENG_IMPORTS],
+  templateUrl: './legal-header.html',
+})
+export class LegalHeader {
+  title = input.required<string>();
+  highlight = input.required<string>();
+  description = input.required<string>();
+  date = input<string>();
+}
