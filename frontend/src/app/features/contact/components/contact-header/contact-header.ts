c8cb6ab770d8a8d2c8cf1bdce15817e24bfa5569
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-19 15:24:11 -0300

    feat: adds contact-header.ts to frontend/src/app/features/contact/components/contact-header/

---
+++ frontend/src/app/features/contact/components/contact-header/contact-header.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-header',
+  imports: [],
+  templateUrl: './contact-header.html',
+})
+export class ContactHeader {}
