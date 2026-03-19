3a0c06930acc5ec808aca9b58c4d48216e5247ba
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-19 15:38:35 -0300

    feat: adds shelters-hero.ts to frontend/src/app/features/shelters/components/shelters-hero/

---
+++ frontend/src/app/features/shelters/components/shelters-hero/shelters-hero.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shelters-hero',
+  imports: [],
+  templateUrl: './shelters-hero.html',
+})
+export class SheltersHero {}
