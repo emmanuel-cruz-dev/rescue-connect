7d6bd8cee1114509d390a569fe352fd475a80d67
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-20 10:50:24 -0300

    feat: adds shelter-card.ts to frontend/src/app/features/shelters/shelter-card/

---
+++ frontend/src/app/features/shelters/components/shelter-card/shelter-card.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+
+import { PRIMENG_IMPORTS } from '../../../../shared/primeng/primeng.imports';
+
+interface Shelter {
+  id: number;
+  name: string;
+  image: string;
+  logo: string;
+  location: string;
+  website: string;
+  description: string;
+}
+
+@Component({
+  selector: 'app-shelter-card',
+  imports: [PRIMENG_IMPORTS],
+  templateUrl: './shelter-card.html',
+})
+export class ShelterCard {
+  @Input({ required: true }) shelter!: Shelter;
+}
