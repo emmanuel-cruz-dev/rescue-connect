39f6b31e12a9bc396a0c0a87c0ac00fcaa8e4843
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-20 11:02:43 -0300

    feat: adds shelters.routes.ts to frontend/src/app/features/shelters/

---
+++ frontend/src/app/features/shelters/shelters.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+import { Shelters } from './pages/shelters/shelters';
+
+export const SHELTERS_ROUTES: Routes = [
+  {
+    path: '',
+    component: Shelters,
+    title: 'Refugios asociados',
+  },
+];
