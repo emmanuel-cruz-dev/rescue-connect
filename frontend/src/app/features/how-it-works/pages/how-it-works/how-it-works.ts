224d07dfaa47f2a7ed19e035241a0f95b9940915
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-21 11:40:47 -0300

    feat: adds how-it-works files to frontend/src/app/features/how-it-works/pages/

---
+++ frontend/src/app/features/how-it-works/pages/how-it-works/how-it-works.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { InfoSection } from '../../components/info-section/info-section';
+
+@Component({
+  selector: 'app-how-it-works',
+  imports: [InfoSection],
+  templateUrl: './how-it-works.html',
+})
+export class HowItWorks {}
