761ad5ef9b710e7123223f6fb5e51566767765a8
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-21 11:48:04 -0300

    feat: adds section-header files to frontend/src/app/shared/components/section-header/

---
+++ frontend/src/app/shared/components/section-header/section-header.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+import { PRIMENG_IMPORTS } from '../../primeng/primeng.imports';
+
+@Component({
+  selector: 'app-section-header',
+  imports: [PRIMENG_IMPORTS],
+  templateUrl: './section-header.html',
+})
+export class SectionHeader {
+  @Input() eyebrow: string = '';
+  @Input() title: string = '';
+  @Input() description: string = '';
+}
