3c8f07762909a42241d3ec0c99e7c252a478b2f8
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-22 09:36:19 -0300

    feat: adds primeng imports to frontend/src/app/features/save-lives/components/benefits/benefits.ts

--- frontend/src/app/features/save-lives/components/benefits/benefits.ts
+++ frontend/src/app/features/save-lives/components/benefits/benefits.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
+import { PRIMENG_IMPORTS } from '../../../../shared/primeng/primeng.imports';
+
 @Component({
   selector: 'app-benefits',
-  imports: [],
+  imports: [PRIMENG_IMPORTS],
   templateUrl: './benefits.html',
 })
 export class Benefits {}
