764d11b45229e1f05d1eb5d57ffd3dadcb5c6031
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-22 09:39:25 -0300

    feat: adds save-lives.ts to frontend/src/app/features/save-lives/

---
+++ frontend/src/app/features/save-lives/save-lives.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+import { SaveLives } from './pages/save-lives/save-lives';
+
+export const SAVE_LIVES_ROUTES: Routes = [
+  {
+    path: '',
+    component: SaveLives,
+  },
+];
