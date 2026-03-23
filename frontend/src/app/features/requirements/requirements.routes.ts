649e3e584cfeb7f5cee468daf454ef36718aab61
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-23 12:47:00 -0300

    feat: adds requirements.routes.ts to frontend/src/app/features/requirements/

---
+++ frontend/src/app/features/requirements/requirements.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+import { Requirements } from './pages/requirements/requirements';
+
+export const REQUIREMENTS_ROUTES: Routes = [
+  {
+    path: '',
+    component: Requirements,
+    title: 'Requisitos',
+  },
+];
