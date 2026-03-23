e61343479d15ba3fa2f825fa3d3ab9e46b06c8f8
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-23 12:44:10 -0300

    feat: adds adoption-requirements.ts to frontend/src/app/features/requirements/components/adoption-requirements/

---
+++ frontend/src/app/features/requirements/components/adoption-requirements/adoption-requirements.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { PRIMENG_IMPORTS } from '../../../../shared/primeng/primeng.imports';
+
+@Component({
+  selector: 'app-adoption-requirements',
+  imports: [PRIMENG_IMPORTS],
+  templateUrl: './adoption-requirements.html',
+})
+export class AdoptionRequirements {}
