d2b8014e82efaae25a2197aa311894a056b1df78
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-24 12:40:21 -0300

    feat: adds primeng imports to frontend/src/app/features/contact/components/contact-header/contact-header.ts

--- frontend/src/app/features/contact/components/contact-header/contact-header.ts
+++ frontend/src/app/features/contact/components/contact-header/contact-header.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
+import { PRIMENG_IMPORTS } from '../../../../shared/primeng/primeng.imports';
+
 @Component({
   selector: 'app-contact-header',
-  imports: [],
+  imports: [PRIMENG_IMPORTS],
   templateUrl: './contact-header.html',
 })
 export class ContactHeader {}
