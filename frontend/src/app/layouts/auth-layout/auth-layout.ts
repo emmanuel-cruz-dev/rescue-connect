4594fc118c6a8e95f8ba2c3581520ae91150d566
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-24 11:03:45 -0300

    feat: adds Footer to frontend/src/app/layouts/auth-layout/auth-layout.ts

--- frontend/src/app/layouts/auth-layout/auth-layout.ts
+++ frontend/src/app/layouts/auth-layout/auth-layout.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
 import { Navbar } from '../../shared/components/navbar/navbar';
+import { Footer } from '../../shared/components/footer/footer';
 import { PRIMENG_IMPORTS } from '../../shared/primeng/primeng.imports';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [RouterModule, Navbar, PRIMENG_IMPORTS],
+  imports: [RouterModule, Navbar, Footer, PRIMENG_IMPORTS],
   templateUrl: './auth-layout.html',
 })
 export class AuthLayout {}
