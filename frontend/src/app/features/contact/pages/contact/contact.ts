ff7c4d3cb2b849fadb5a5b64ffe4e71ba6bcab13
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-24 12:49:42 -0300

    feat: adds SocialLinksItem to frontend/src/app/features/contact/pages/contact/contact.ts

--- frontend/src/app/features/contact/pages/contact/contact.ts
+++ frontend/src/app/features/contact/pages/contact/contact.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
-import { PRIMENG_IMPORTS } from '../../../../shared/primeng/primeng.imports';
 import { ContactHeader } from '../../components/contact-header/contact-header';
 import { ContactForm } from '../../components/contact-form/contact-form';
+import { SocialLinksItem } from '../../../../shared/components/social-links-item/social-links-item';
+import { PRIMENG_IMPORTS } from '../../../../shared/primeng/primeng.imports';
 
 @Component({
   selector: 'app-contact',
-  imports: [ContactHeader, ContactForm, PRIMENG_IMPORTS],
+  imports: [ContactHeader, ContactForm, SocialLinksItem, PRIMENG_IMPORTS],
   templateUrl: './contact.html',
 })
 export class Contact {}
