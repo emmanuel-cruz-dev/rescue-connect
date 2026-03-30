d9a17105a5c67ade3b141b66658cea0fe2f79341
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-30 09:51:24 -0300

    feat: adds protection-law files to frontend/src/app/features/report/components/protection-law/

---
+++ frontend/src/app/features/report/components/protection-law/protection-law.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-protection-law',
+  imports: [],
+  templateUrl: './protection-law.html',
+})
+export class ProtectionLaw {}
