0b661bf1395cc45692fcedd8e3f93302dd6d5aa8
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-30 09:52:49 -0300

    feat: adds report.routes.ts to frontend/src/app/features/report/

---
+++ frontend/src/app/features/report/report.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+import { Report } from './pages/report/report';
+
+export const REPORT_ROUTES: Routes = [
+  {
+    path: '',
+    component: Report,
+    title: 'Denunciar',
+  },
+];
