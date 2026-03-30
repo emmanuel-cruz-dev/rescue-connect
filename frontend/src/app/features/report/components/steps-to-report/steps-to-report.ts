7e9a9fbef03f6ca549b3dd23b9ba09dc9fd9633e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-03-30 09:47:40 -0300

    feat: adds steps-to-report files to frontend/src/app/features/report/components/steps-to-report/

---
+++ frontend/src/app/features/report/components/steps-to-report/steps-to-report.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-steps-to-report',
+  imports: [],
+  templateUrl: './steps-to-report.html',
+})
+export class StepsToReport {}
