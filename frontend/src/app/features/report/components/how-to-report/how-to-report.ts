602878fdae6d16ba870fec55cc60953d270b078d
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-03 12:02:51 -0300

    feat: modifies attributes and methods in frontend/src/app/features/report/components/how-to-report/how-to-report.ts

--- frontend/src/app/features/report/components/how-to-report/how-to-report.ts
+++ frontend/src/app/features/report/components/how-to-report/how-to-report.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
 
+import { PRIMENG_IMPORTS } from '../../../../shared';
+
+interface ReportStep {
+  icon: string;
+  title: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-how-to-report',
-  imports: [],
+  imports: [PRIMENG_IMPORTS],
   templateUrl: './how-to-report.html',
 })
-export class HowToReport {}
+export class HowToReport {
+  reportSteps: ReportStep[] = [
+    {
+      icon: 'pi-megaphone',
+      title: 'Delito de acción pública',
+      description:
+        'Cualquiera puede denunciar mientras sea mayor de 18 años y acredite su identidad.',
+    },
+    {
+      icon: 'pi-hammer',
+      title: 'Protección legal',
+      description: 'No se necesitan mayores formalidades para hacerlo.',
+    },
+    {
+      icon: 'pi-clock',
+      title: 'Proceso simple',
+      description: 'Podés realizar una denuncia en pocos pasos y sin trámites complejos.',
+    },
+    {
+      icon: 'pi-bolt',
+      title: 'Impacto real',
+      description: 'Tu denuncia puede salvar una vida.',
+    },
+  ];
+}
