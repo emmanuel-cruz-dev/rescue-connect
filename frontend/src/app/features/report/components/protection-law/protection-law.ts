b843d0bab8246c0ff6c54f13830a3d4ea9da4528
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-03 12:15:56 -0300

    feat: adds imports and attributes to frontend/src/app/features/report/components/protection-law/protection-law.ts

--- frontend/src/app/features/report/components/protection-law/protection-law.ts
+++ frontend/src/app/features/report/components/protection-law/protection-law.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
 
+import { PRIMENG_IMPORTS } from '../../../../shared';
+
+interface ProtectionItem {
+  icon: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-protection-law',
-  imports: [],
+  imports: [PRIMENG_IMPORTS],
   templateUrl: './protection-law.html',
 })
-export class ProtectionLaw {}
+export class ProtectionLaw {
+  protectionItems: ProtectionItem[] = [
+    {
+      icon: 'pi-exclamation-circle',
+      description: 'No alimentar a los animales de manera adecuada.',
+    },
+    {
+      icon: 'pi-exclamation-circle',
+      description: 'Lastimarlos intencionalmente o causarles sufrimientos innecesarios.',
+    },
+    {
+      icon: 'pi-exclamation-circle',
+      description:
+        'Lastimar y arrollar animales intencionalmente, causarles torturas o sufrimientos innecesarios o matarlos por sólo espíritu de perversidad.',
+    },
+  ];
+}
