63aa2b9db8c75d068684e52dd02c91e219c06bf5
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-06 12:32:12 -0300

    feat: adds primeng imports to frontend/src/app/features/report/components/steps-to-report/steps-to-report.ts

--- frontend/src/app/features/report/components/steps-to-report/steps-to-report.ts
+++ frontend/src/app/features/report/components/steps-to-report/steps-to-report.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
+import { PRIMENG_IMPORTS } from '../../../../shared';
+
 @Component({
   selector: 'app-steps-to-report',
-  imports: [],
+  imports: [PRIMENG_IMPORTS],
   templateUrl: './steps-to-report.html',
 })
 export class StepsToReport {}
