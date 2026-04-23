4f705b8d654ffafa0a25309973a04dde3a46d218
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-23 12:25:27 -0300

    feat: adds router link and primeng imports to frontend/src/app/shared/components/why-adopt/why-adopt.ts

--- frontend/src/app/shared/components/why-adopt/why-adopt.ts
+++ frontend/src/app/shared/components/why-adopt/why-adopt.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { PRIMENG_IMPORTS } from '../../primeng';
 
 @Component({
   selector: 'app-why-adopt',
-  imports: [],
+  imports: [RouterLink, PRIMENG_IMPORTS],
   templateUrl: './why-adopt.html',
 })
 export class WhyAdopt {}
