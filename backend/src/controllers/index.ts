e5217a602e40a9100bd00aca8cf9466372a52579
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-23 11:29:37 -0300

    feat: adds index.ts to backend/src/controllers/

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,6 @@
+export { default as authController } from "./auth.controller";
+export { default as usersController } from "./users.controller";
+export { default as petsController } from "./pets.controller";
+export { default as adoptionController } from "./adoption.controller";
+export { default as adminController } from "./admin.controller";
+export { default as notificationController } from "./notification.controller";
