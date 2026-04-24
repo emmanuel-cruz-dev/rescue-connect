300f0e5c8e5f45380c7fe7d12d5f00944e6b45ea
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-24 09:53:26 -0300

    feat: adds WhyAdopt to lading files in frontend/src/app/features/home/pages/

--- frontend/src/app/features/home/pages/landing.ts
+++ frontend/src/app/features/home/pages/landing.ts
@@ -5,10 +5,11 @@ import { FeaturedPets } from '../components/featured-pets/featured-pets';
 import { Testimonials } from '../components/testimonials/testimonials';
 import { Adopt } from '../components/adopt/adopt';
 import { Faq } from '../components/faq/faq';
+import { WhyAdopt } from '../../../shared';
 
 @Component({
   selector: 'app-landing',
-  imports: [HeroSection, FeaturedPets, Testimonials, Adopt, Faq],
+  imports: [HeroSection, FeaturedPets, Testimonials, Adopt, Faq, WhyAdopt],
   templateUrl: './landing.html',
 })
 export class Landing {}
