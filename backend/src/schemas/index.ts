f7af997dfa1a7606f49638e8b2e6bde51a9f826b
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-04-24 10:27:30 -0300

    feat: adds index.ts to backend/src/schemas/

---
+++ backend/src/schemas/index.ts
@@ -0,0 +1,4 @@
+export { default as adoptionRequestModel } from "./adoption.schema";
+export { default as petModel } from "./pets.schema";
+export { default as userModel } from "./user.schema";
+export { default as notificationModel } from "./notification.schema";
